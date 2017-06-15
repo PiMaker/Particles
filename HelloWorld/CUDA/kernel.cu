a416eb4fdd84fc1049a5c35db1c6ac4956755aa0
Stefan Reiter <stefan.reiter@studierende.htl-donaustadt.at>
2017-06-15 20:47:21 +0200

    First prototype of Hello World

--- HelloWorld/CUDA/kernel.cu
+++ HelloWorld/CUDA/kernel.cu
@@ -1,15 +1,147 @@
+#include "cuda.h"
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "cublas.h"
 #include <stdio.h>
 
-__global__ void kernel(int* a, int* b, int* out, int N)
+#define SIZE 10
+#define Active pi
+#define X pi + 1
+#define Y pi + 2
+#define PrevX pi + 3
+#define PrevY pi + 4
+#define Radius pi + 5
+
+inline __device__ float2 operator-(float2 a, float2 b)
 {
-	int i = blockDim.x * blockIdx.x + threadIdx.x;
-	if (i < N)
-		out[i] = a[i] + b[i];
+	return make_float2(a.x - b.x, a.y - b.y);
+}
+inline __device__ float2 operator*(float2 a, float2 b)
+{
+	return make_float2(a.x * b.x, a.y * b.y);
+}
+inline __device__ float2 operator*(float2 a, float b)
+{
+	return make_float2(a.x * b, a.y * b);
+}
+inline __device__ float rsqrtf2(float x)
+{
+	return 1.0f / sqrtf(x);
+}
+inline __device__ float dot(float2 a, float2 b)
+{
+	return a.x * b.x + a.y * b.y;
+}
+inline __device__ float2 normalize(float2 v)
+{
+	float invLen = rsqrtf2(dot(v, v));
+	return v * invLen;
+}
+
+__global__ void kernel(float* p, float* out, int N, float delta, float* area)
+{
+	int particle = blockDim.x * blockIdx.x + threadIdx.x;
+	int pi = particle * SIZE;
+	bool valid = particle < N && p[Active] > 0;
+
+	if (valid)
+	{
+		// == Gravity ==
+		//p[Y] += 0.0005 * delta;
+
+		float2 c = make_float2(area[0] + area[2] / 2, area[1] + area[3] / 2);
+		float2 pos = make_float2(p[X], p[Y]);
+		float2 diff = c - pos;
+		float2 gravity = normalize(diff) * 0.0005 * delta;
+		p[Y] += gravity.y;
+		p[X] += gravity.x;
+	}
+
+	// == Sync ==
+	__syncthreads();
+
+	if (valid)
+	{
+		// == Collision (naive) ==
+		for (int j = 0; j < N; j++)
+		{
+			int pi2 = j * SIZE;
+
+			if (pi == pi2)
+			{
+				continue;
+			}
+
+			float x = p[X] - p[pi2 + 1];
+			float y = p[Y] - p[pi2 + 2];
+			float slength = x*x + y*y;
+			float length = sqrtf(slength);
+			float target = p[Radius] + p[pi2 + 5];
+
+			if (length < target)
+			{
+				float factor = (length - target) / length;
+				p[X] -= x*factor*0.5;
+				p[Y] -= y*factor*0.5;
+				p[pi2 + 1] += x*factor*0.5;
+				p[pi2 + 2] += y*factor*0.5;
+			}
+		}
+
+
+		// == Border Collision ==
+			
+		// Bottom
+		if (p[Y] + p[Radius] > area[3])
+		{
+			p[Y] = area[3] - p[Radius];
+		}
+
+		// Top
+		if (p[Y] - p[Radius] < area[1])
+		{
+			p[Y] = area[1] + p[Radius];
+		}
+
+		// Left
+		if (p[X] - p[Radius] < area[0])
+		{
+			p[X] = area[0] + p[Radius];
+		}
+
+		// Right
+		if (p[X] + p[Radius] > area[2])
+		{
+			p[X] = area[2] - p[Radius];
+		}
+
+
+		// == Inertia ==
+		float x = p[X] + (p[X] - p[PrevX]);
+		float y = p[Y] + (p[Y] - p[PrevY]);
+		out[PrevX] = p[X];
+		out[PrevY] = p[Y];
+		out[X] = x;
+		out[Y] = y;
+	}
 }
 
 int main()
 {
 	return 0;
 }
+
+/*
+
+p - Memory layout:
+
+i = particle index * 10
+i = active if > 0
+i + 1 = Position X
+i + 2 = Position Y
+i + 3 = Prev. Position X
+i + 4 = Prev. Position Y
+i + 5 = Radius
+i + 6/7/8/9 = Color (RGBA)
+
+*/
--- HelloWorld/HelloWorld/BaseGame.cs
+++ HelloWorld/HelloWorld/BaseGame.cs
@@ -4,38 +4,44 @@
 // See <summary> tags for more information.
 
 using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using ManagedCuda;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Shapes;
 
 namespace HelloWorld
 {
     public class BaseGame : Game
     {
-        private const int VECTOR_SIZE = 5120;
+        private const bool FIXED_TIMESTEP = false;
+
+        private const int PARTICLES = 4000;
+        private const int PARTICLE_SIZE = 10;
+        private const float RADIUS = 4;
+
         private const int THREADS_PER_BLOCK = 1024;
+        private const int ITERATIONS = 1;
 
-        private static CudaKernel addTwoVectorWithCuda;
+        private static readonly Random random = new Random();
+        private static readonly Polygon circle = BaseGame.CreateCircle(BaseGame.RADIUS, 6);
+        private readonly FrameCounter frameCounter = new FrameCounter();
+        private readonly GraphicsDeviceManager graphics;
+        private readonly float[] result = new float[BaseGame.PARTICLES * BaseGame.PARTICLE_SIZE];
+        private readonly FrameCounter updateCounter = new FrameCounter();
 
-        private static readonly Func<int[], int[], int, int[]> addVectors = (a, b, size) =>
-        {
-            // init parameters
-            CudaDeviceVariable<int> vectorHostA = a;
-            CudaDeviceVariable<int> vectorHostB = b;
-            var vectorHostOut = new CudaDeviceVariable<int>(size);
-            // run cuda method
-            BaseGame.addTwoVectorWithCuda.Run(vectorHostA.DevicePointer, vectorHostB.DevicePointer,
-                vectorHostOut.DevicePointer, size);
-            // copy return to host
-            var output = new int[size];
-            vectorHostOut.CopyToHost(output);
-            return output;
-        };
-
-        private GraphicsDeviceManager graphics;
+        private SpriteFont font;
+        private CudaKernel kernel;
+        private TimeSpan lastTickTime = TimeSpan.Zero;
+        private float maxInfoTextWidth;
+        private CudaDeviceVariable<float> particleMemory1;
+        private CudaDeviceVariable<float> particleMemory2;
         private SpriteBatch spriteBatch;
+        private CudaDeviceVariable<float> area;
 
         public BaseGame()
         {
@@ -45,16 +51,28 @@ namespace HelloWorld
 
         protected override void Initialize()
         {
-            base.Initialize();
+            this.graphics.SynchronizeWithVerticalRetrace = BaseGame.FIXED_TIMESTEP;
+            this.IsFixedTimeStep = BaseGame.FIXED_TIMESTEP;
+            this.graphics.PreferredBackBufferWidth = 1600;
+            this.graphics.PreferredBackBufferHeight = 900;
+            this.graphics.ApplyChanges();
             
-            // TEST
-            Test();
+            this.InitKernels();
+            this.InitParticles();
+
+            // Coordinates: x, y, x_far, y_far
+            this.area = new CudaDeviceVariable<float>(4);
+            this.area.CopyToDevice(new float[] { 0, 0, this.graphics.PreferredBackBufferWidth, this.graphics.PreferredBackBufferHeight });
+
+            base.Initialize();
         }
 
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+
+            this.font = this.Content.Load<SpriteFont>("font");
         }
 
         protected override void UnloadContent()
@@ -63,42 +81,154 @@ namespace HelloWorld
 
         protected override void Update(GameTime gameTime)
         {
+            this.updateCounter.Update((float) gameTime.ElapsedGameTime.TotalSeconds);
+
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
                 this.Exit();
             }
 
+            var now = DateTime.Now;
+
+            // Update routine
+
+            // Prepare steps
+            var delta = (float)gameTime.ElapsedGameTime.TotalMilliseconds/BaseGame.ITERATIONS;
+
+            for (var i = 0; i < BaseGame.ITERATIONS; i++)
+            {
+                // Call kernel
+                this.kernel.Run(this.particleMemory1.DevicePointer, this.particleMemory2.DevicePointer, BaseGame.PARTICLES, delta, this.area.DevicePointer);
+
+                // Swap memory pointers
+                var temp = this.particleMemory2;
+                this.particleMemory2 = this.particleMemory1;
+                this.particleMemory1 = temp;
+            }
+
+            // Retrieve result
+            this.particleMemory1.CopyToHost(this.result);
+
+            this.lastTickTime = DateTime.Now - now;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            this.frameCounter.Update((float) gameTime.ElapsedGameTime.TotalSeconds);
 
+            this.GraphicsDevice.Clear(Color.White);
+            this.spriteBatch.Begin();
+
+            // Draw center point
+            this.spriteBatch.DrawCircle(new Vector2(this.graphics.PreferredBackBufferWidth/2f, this.graphics.PreferredBackBufferHeight/2f), 10, 32, Color.Black, 3f);
+
+            // Draw particles
+            for (var i = 0; i < BaseGame.PARTICLES; i++)
+            {
+                var pi = i * BaseGame.PARTICLE_SIZE;
+                if (this.result[pi] > 0)
+                {
+                    this.spriteBatch.DrawPolygon(new Vector2(this.result[pi + 1], this.result[pi + 2]), BaseGame.circle,
+                        BaseGame.FromFloatArray(this.result, pi + 6));
+                }
+            }
+
+            // Draw Infotext
+            var text =
+                $"Tick: {Math.Round(this.lastTickTime.TotalMilliseconds, 2)} ms\nUPS: {Math.Round(this.updateCounter.AverageFramesPerSecond, 2)}\nFPS: {Math.Round(this.frameCounter.AverageFramesPerSecond, 2)}\nParticles: {BaseGame.PARTICLES}";
+            var size = this.font.MeasureString(text);
+            size.X = this.maxInfoTextWidth = Math.Max(this.maxInfoTextWidth, size.X);
+            var position = Vector2.One * 5;
+
+            this.spriteBatch.FillRectangle(Vector2.Zero, size + position * 2, Color.LightGray * 0.7f);
+            this.spriteBatch.DrawString(this.font, text, position, Color.Green);
+
+            this.spriteBatch.End();
             base.Draw(gameTime);
         }
 
-        private static void InitKernels()
+        private void InitKernels()
         {
+            var kernelName = File.ReadAllText("kernel.ptx");
+            var match = Regex.Match(kernelName, @"\/\/ \.globl\s(.*?)\r?\n");
+            kernelName = match.Groups[1].Value;
+
             var cntxt = new CudaContext();
-            var cumodule =
-                cntxt.LoadModule(
-                    @"kernel.ptx");
-            BaseGame.addTwoVectorWithCuda = new CudaKernel("_Z6kernelPiS_S_i", cumodule, cntxt);
-            BaseGame.addTwoVectorWithCuda.BlockDimensions = BaseGame.THREADS_PER_BLOCK;
-            BaseGame.addTwoVectorWithCuda.GridDimensions = BaseGame.VECTOR_SIZE / BaseGame.THREADS_PER_BLOCK + 1;
+            var cumodule = cntxt.LoadModule(@"kernel.ptx");
+            this.kernel = new CudaKernel(kernelName, cumodule, cntxt)
+            {
+                BlockDimensions = BaseGame.THREADS_PER_BLOCK,
+                GridDimensions = BaseGame.PARTICLES / BaseGame.THREADS_PER_BLOCK + 1
+            };
         }
 
-        private static void Test()
+        private void InitParticles()
         {
-            BaseGame.InitKernels();
-            var vectorA = Enumerable.Range(1, BaseGame.VECTOR_SIZE).ToArray();
-            var vectorB = Enumerable.Range(1, BaseGame.VECTOR_SIZE).ToArray();
-            var vector = BaseGame.addVectors(vectorA, vectorB, BaseGame.VECTOR_SIZE);
-            for (var i = 0; i < BaseGame.VECTOR_SIZE; i++)
+            var particles = new float[BaseGame.PARTICLES * BaseGame.PARTICLE_SIZE];
+
+            for (var i = 0; i < BaseGame.PARTICLES; i++)
             {
-                Console.WriteLine("{0}+{1}={2}", vectorA[i], vectorB[i], vector[i]);
+                var pi = i * BaseGame.PARTICLE_SIZE;
+                particles[pi] = 1;
+
+                // Random position
+                particles[pi + 1] =
+                    particles[pi + 3] = (BaseGame.random.NextSingle(0.05f, 0.95f)) * this.graphics.PreferredBackBufferWidth;
+                particles[pi + 2] =
+                    particles[pi + 4] = BaseGame.random.NextSingle(0.05f, 0.95f) * this.graphics.PreferredBackBufferHeight;
+
+                // Radius
+                particles[pi + 5] = BaseGame.RADIUS;
+
+                // Color
+                particles[pi + 6] = 1;
+                particles[pi + 9] = 1;
             }
+
+            // Initialize memory1 and memory2 with actual particles
+            this.particleMemory1 = new CudaDeviceVariable<float>(BaseGame.PARTICLES * BaseGame.PARTICLE_SIZE);
+            this.particleMemory1.CopyToDevice(particles);
+            this.particleMemory2 = new CudaDeviceVariable<float>(BaseGame.PARTICLES * BaseGame.PARTICLE_SIZE);
+            this.particleMemory2.CopyToDevice(particles);
+        }
+
+        // From Monogame.Extended
+        private static Polygon CreateCircle(double radius, int sides)
+        {
+            var vectors = new List<Vector2>();
+
+            const double MAX = 2.0 * Math.PI;
+            var step = MAX / sides;
+
+            for (var theta = 0.0; theta < MAX; theta += step)
+            {
+                vectors.Add(new Vector2((float) (radius * Math.Cos(theta)), (float) (radius * Math.Sin(theta))));
+            }
+
+            // then add the first vector again so it's a complete loop
+            vectors.Add(new Vector2((float) (radius * Math.Cos(0)), (float) (radius * Math.Sin(0))));
+            return new Polygon(vectors);
+        }
+
+        private static Color FromFloatArray(float[] arr, int i)
+        {
+            return new Color(arr[i], arr[i + 1], arr[i + 2], arr[i + 3]);
         }
     }
 }
+
+/*
+
+p - Memory layout:
+
+i = particle index * 10
+i = active if > 0
+i + 1 = Position X
+i + 2 = Position Y
+i + 3 = Prev. Position X
+i + 4 = Prev. Position Y
+i + 5 = Radius
+i + 6/7/8/9 = Color (RGBA)
+
+*/
